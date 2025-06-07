dfd4f30f154fbc3b8530dad4e1a0a177a9adee5e
PintilieGeani <pintilie.geany@yahoo.it>
2025-06-07 17:03:48 +0200

    feat: done-no-css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const copyArray = [...blogList, newBlogTitle];
+    const copyArray = [...blogList, {titolo: newBlogTitle}];
     setBlogList(copyArray)
     
   }
