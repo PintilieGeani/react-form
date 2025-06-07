f689f9fb69b03079093ce0e28ad40db2575b8b98
PintilieGeani <pintilie.geany@yahoo.it>
2025-06-07 17:02:08 +0200

    feat: bloccato

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import blogPosts from "./data.js"
+import { useState } from "react"
 
 function App() {
-  
+  const [newBlogTitle, setNewblogTitle] = useState("");
+  const[blogList, setBlogList] = useState(blogPosts);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const copyArray = [...blogList, newBlogTitle];
+    setBlogList(copyArray)
+    
+  }
 
   return (
     <>
      <div>
       <ul>
-        {blogPosts.map((curPost) => <li>{curPost.titolo}</li>)}
+        {blogList.map((curPost, index) => <li key={index}>{curPost.titolo}</li>)}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="titolo">Inserisci il tuo titolo</label>
+        <input autoComplete="off" type="text"  id="titolo" value={newBlogTitle} onChange={(event) => setNewblogTitle(event.target.value)}/>
+        <button>Aggiungi Titolo</button>
+      </form>
      </div>
     </>
   )
